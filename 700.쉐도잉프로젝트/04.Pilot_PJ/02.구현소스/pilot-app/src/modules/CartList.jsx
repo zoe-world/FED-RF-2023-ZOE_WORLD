96ed665157ff98452bee600735390efe3fd8a7fa
zoe <joheun2390@gmail.com>
2023-12-05 11:25:03 +0900

    20231205

---
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
@@ -0,0 +1,11 @@
+// Pilot PJ 장바구니 리스트 컴포넌트 
+
+// 장바구니 리스트 css 불러오기
+import "../css/cartlist.css";
+export function CartList(){
+    return(
+        <>
+        
+        </>
+    )
+} //////////CartList 컴포넌트 //////
