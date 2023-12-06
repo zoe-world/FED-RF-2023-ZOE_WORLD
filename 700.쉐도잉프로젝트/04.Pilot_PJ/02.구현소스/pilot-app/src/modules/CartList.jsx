2e1813951f72f04a69bb055030c2590331126cba
zoe <joheun2390@gmail.com>
2023-12-06 18:23:38 +0900

    20231206

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
@@ -1,11 +1,78 @@
-// Pilot PJ 장바구니 리스트 컴포넌트 
+// Pilot PJ 장바구니 리스트 컴포넌트
 
-// 장바구니 리스트 css 불러오기
+// 장바구니 리스트 CSS 불러오기
 import "../css/cartlist.css";
-export function CartList(){
-    return(
-        <>
-        
-        </>
-    )
-} //////////CartList 컴포넌트 //////
+
+export function CartList() {
+  // 선택 데이터 : 로컬스토리지 데이터를 객체변환!
+  const selData = JSON.parse(localStorage.getItem("cart"));
+
+  console.log(selData);
+
+  //정규식함수(숫자 세자리마다 콤마해주는 기능)
+  function addComma(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  return (
+    <>
+      <section id="cartlist">
+        <a href="#" className="cbtn cbtn2">
+          <span>닫기버튼</span>
+        </a>
+        <table>
+          <caption>
+            <h1> 카트 리스트</h1>
+          </caption>
+          <tbody>
+            <tr>
+              <th>상품</th>
+              <th>번호</th>
+              <th>상품명</th>
+              <th>상품코드</th>
+              <th>단가</th>
+              <th>수량</th>
+              <th>합계</th>
+              <th>삭제</th>
+            </tr>
+
+            {selData.map((v, i) => (
+              <tr key={i}>
+                {/* 상품이미지 */}
+                <td>
+                  <img
+                    src={"images/goods/" + v.cat + "/" + v.ginfo[0] + ".png"}
+                    alt="item"
+                  />
+                </td>
+                {/* 리스트순번 */}
+                <td>{i + 1}</td>
+                {/* 상품명 */}
+                <td>{v.ginfo[1]}</td>
+                {/* 상품코드 */}
+                <td>{v.ginfo[2]}</td>
+                {/* 상품가격 */}
+                <td>{addComma(v.ginfo[3])}원</td>
+                {/* 상품수량 */}
+                <td>{v.num}</td>
+                {/* 상품가격 총합계 */}
+                <td>{addComma(v.ginfo[3] * v.num)}원</td>
+                <td>
+                  <button className="cfn" data-idx={v.idx}>
+                    ×
+                  </button>
+                </td>
+              </tr>
+            ))}
+
+            <tr>
+              <td colSpan="6">총합계 :</td>
+              <td>999,000원</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </>
+  );
+} ////////////// CartList 컴포넌트 /////////
